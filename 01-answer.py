e3ae3bb882668495b50fd6434d2ab66ccacf5323
heyimrendy <61161733+heyimrendy@users.noreply.github.com>
2020-12-02 00:05:37 +0000

    day1

---
+++ 01-answer.py
@@ -0,0 +1,54 @@
+with open("./01-input.txt") as f:
+    txt_file = f.readlines()
+
+def part_one():
+    done = False
+    final_anwer = 0
+    for i, content in enumerate(txt_file):
+        for l, content2 in enumerate(txt_file):
+            if i == l:
+                continue
+
+            if int(content.strip()) + int(content2.strip()) == 2020:
+                final_anwer = int(content.strip()) * int(content2.strip())
+                done = True
+                break
+        
+        if done:
+            break
+
+    print('Part 1 final answer: {}'.format(final_anwer))
+
+def part_two():
+    done_root = False
+    done_child = False
+    final_anwer = 0
+    for i, content in enumerate(txt_file):
+        for l, content2 in enumerate(txt_file):
+            if i == l:
+                continue
+
+            for m, content3 in enumerate(txt_file):
+                if i == m and l == m:
+                    continue
+
+                if int(content.strip()) + int(content2.strip()) + int(content3.strip()) == 2020:
+                    final_anwer = int(content.strip()) * int(content2.strip()) * int(content3.strip())
+                    done_root = True
+                    done_child = True
+                    break
+            
+            if done_child:
+                break
+        
+        if done_root:
+            break
+
+    print('Part 2 final answer: {}'.format(final_anwer))
+
+def main():
+    part_one()
+    part_two()
+
+if __name__ == "__main__":
+    main()
